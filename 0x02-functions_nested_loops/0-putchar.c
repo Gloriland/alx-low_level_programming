b7985013abcd8d3e71daf34c5454c53df144aa25
Gloriland <princegloriland@outlook.com>
2022-03-15 18:54:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, gz;
+	int count, sz;
 
-	gz = sizeof(str) / sizeof(int);
-	for (count = 0; count < gz; count++)
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 
 	{
 		_putchar(str[count]);
