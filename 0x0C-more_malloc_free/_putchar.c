1821786567ec9581182f6b1aaa1181224bab084f
Gloriland <princegloriland@outlook.com>
2022-04-07 15:58:13 -0700

    putchar

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,10 +1,13 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#include <unistd.h>
 
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-#endif
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -! is returned, amd errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1);
+}
